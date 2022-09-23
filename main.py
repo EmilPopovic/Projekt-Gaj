7a3b68b91c9a103e62a4e78f3830baffeb1a687d
Mjolnir2425 <104315710+Mjolnir2425@users.noreply.github.com>
2022-09-23 15:01:32 +0200

    popravljen main
    stari main nije radio, ovaj main radi, a main je početak svega

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import asyncio
-from discord.ext import commands
+import music_cog as mc
 from discord_components import ComponentsBot
 from help_cog import help_cog
-from music_cog import music_cog
 from anti_spam_cog import anti_spam_cog
 from secrets import token
 
@@ -13,32 +12,32 @@ bot = ComponentsBot(command_prefix='!')
 @bot.event
 async def on_ready():
     print(f'\nLogged in as {bot.user}\nBOT START\n')
-
-
-async def create_music_cog(bot, guild):
-    music_cog = music_cog(bot, guild)
-    await music_cog._init()
-    return music_cog
-
-
-async def main():
-    '''Main je početak svega.'''
+    print(f'Bot user id: {bot.user.id}')
+    
     bot.remove_command('help')
 
     bot.add_cog(help_cog(bot))
 
-    print(bot.guilds)
-
     guilds = [guild.id for guild in bot.guilds]
     for guild in guilds:
+        print(guild)
+
         m_cog = await create_music_cog(bot, guild)
         bot.add_cog(m_cog)
 
     bot.add_cog(anti_spam_cog(bot))
 
+
+async def create_music_cog(bot, guild):
+    music_cog = mc.music_cog(bot, guild)
+    await music_cog._init()
+    return music_cog
+
+
+def main():
+    '''Main je početak svega.'''
     bot.run(token)
 
 
 if __name__ ==  '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    main()
