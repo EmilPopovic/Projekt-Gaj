4e40af3d8b8572d9f524d9a7f8f0da4b86e7853c
Mjolnir2425 <104315710+Mjolnir2425@users.noreply.github.com>
2022-09-25 14:33:15 +0200

    dodan token

--- secrets.py
+++ secrets.py
@@ -6,6 +6,7 @@ spotify_token = 'BQC2ELMxqCjdWifOiMGHNYUQcjzhI34pZQ0QBqBTrrR0ZQMroHTrXne3Yf1i7Fr
 spotify_user_id = 't0oc9zi3n4vvkwpu5lwlc6rj2'
 refresh_token = 'AQBSko9dVula6DPI6kjGCINpRpT6sxeIideTShzvADCBfwUs4oeNceBNCfsbcTMqS-BITKtpF6enEp8ZrggMmU2OUzbnLEggYSvovhcEa9pbCtSWfUL1IJ7zpjJq5uJKriY'
 base_64 = 'N2NkYTk0NWYxYzQ1NDNjM2E4ZjhiMjZjZWJkYmEzMGM6MTZiZDBlZjQwNzM0NGVhYWFmOGNjNTMwMTc5OTE5NDY='
+token = 'ODMxMjQxOTAxNjI5MjQzMzky.GwppdY.WAFE5piqf0YLzL4NGfP0Ty7hmrvUdxskx8ZsUo'
 
 #https://accounts.spotify.com/authorize?client_id=7cda945f1c4543c3a8f8b26cebdba30c&response_type=code&redirect_uri=https%3A%2F%2Fexample.com&scope=playlist-modify-public%20playlist-modify-private
 #curl -H "Authorization: Basic N2NkYTk0NWYxYzQ1NDNjM2E4ZjhiMjZjZWJkYmEzMGM6MTZiZDBlZjQwNzM0NGVhYWFmOGNjNTMwMTc5OTE5NDY=" -d grant_type=authorization_code -d code=AQAlSMvWaTA6FXJV2F_kQdO7PW-ioP09emm3oGm7HexvOYEtvSxGozx0xzMuv7UVa8inEdWKEOwk5N7L6dYT93YGhFXtZcy3m0thAGONMGbDcpPN6spVaSE439I-AgR-ZPy7e_seZ-cO4eWclOGi327LnExhxFAdVFCQcrPEYAA34C4JwR43AKM4cSrfBZ6-w_zwSPvkfD67hSpY9BnkDHc41d-lL3Dd -d redirect_uri=https%3A%2F%2Fexample.com https://accounts.spotify.com/api/token
