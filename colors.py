b9b725a908d5ccfa4cbb1fd36d75bebd2253e51e
Mjolnir2425 <104315710+Mjolnir2425@users.noreply.github.com>
2022-12-05 00:25:52 +0100

    comments now look god

--- colors.py
+++ colors.py
@@ -1,3 +1,5 @@
+# last changed 05/12/22
+
 from datetime import datetime
 
 
@@ -5,19 +7,19 @@ class Colors:
     """ ANSI color codes """
     BLACK = "\033[0;30m"
     RED = "\033[0;31m"
-    GREEN = "\033[0;32m" # on login
+    GREEN = "\033[0;32m"  # on login
     BROWN = "\033[0;33m"
     BLUE = "\033[0;34m"
-    PURPLE = "\033[0;35m" # channel id
-    CYAN = "\033[0;36m" # user id
+    PURPLE = "\033[0;35m"  # channel id
+    CYAN = "\033[0;36m"  # user id
     LIGHT_GRAY = "\033[0;37m"
-    DARK_GRAY = "\033[1;30m" # timestamps
+    DARK_GRAY = "\033[1;30m"  # timestamps
     LIGHT_RED = "\033[1;31m"
     LIGHT_GREEN = "\033[1;32m"
     YELLOW = "\033[1;33m"
     LIGHT_BLUE = "\033[1;34m"
-    LIGHT_PURPLE = "\033[1;35m" # guild id
-    LIGHT_CYAN = "\033[1;36m" # event
+    LIGHT_PURPLE = "\033[1;35m"  # guild id
+    LIGHT_CYAN = "\033[1;36m"  # event
     LIGHT_WHITE = "\033[1;37m"
     BOLD = "\033[1m"
     FAINT = "\033[2m"
