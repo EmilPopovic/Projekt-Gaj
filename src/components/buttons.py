c96b38cf5a35b735ae797ce8045c8cab0fa77817
Mjolnir2425 <104315710+Mjolnir2425@users.noreply.github.com>
2023-02-21 02:39:21 +0100

    2023/02/21 update

--- src/components/buttons.py
+++
@@ -1,8 +0,0 @@
-import discord
-
-
-class BtnStyle:
-    grey  = discord.ButtonStyle.grey
-    green = discord.ButtonStyle.green
-    red   = discord.ButtonStyle.red
-    link  = discord.ButtonStyle.link
