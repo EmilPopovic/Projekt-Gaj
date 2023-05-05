2c343b803fbc53e8458f4caf418f53ce1c6bb6d8
OvajStup <dorijanzlodi@gmail.com>
2023-05-05 23:24:16 +0200

    Wrong push correction

--- api/__init__.py
+++
@@ -1,3 +0,0 @@
-from .genius import GeniusInfo
-from .spotify import SpotifyInfo, SpotifySong, Author
-from .youtube import YouTubeInfo
--- utils/button_style.py
+++
@@ -1,9 +0,0 @@
-import discord
-
-
-class BtnStyle:
-    grey = discord.ButtonStyle.grey
-    green = discord.ButtonStyle.green
-    red = discord.ButtonStyle.red
-    link = discord.ButtonStyle.link
-    blue = discord.ButtonStyle.blurple
